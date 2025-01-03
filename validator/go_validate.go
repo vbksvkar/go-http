e4f73ce882575560b1bf7172b73aaf4f4c20c484
Vijay Karajgikar <vijay.karajgikar@gmail.com>
2025-01-03 13:45:31 -0600

    added govalidate

---
+++ validator/go_validate.go
@@ -0,0 +1,21 @@
+package govalidate
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func NewValidator(tagNameToRegister string) *validator.Validate {
+	validate := validator.New()
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get(tagNameToRegister), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+
+	return validate
+}
